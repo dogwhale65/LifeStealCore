67ab7e4eb2daf030fdedcd67d0e16c592a7748da
dogwhale65 <willmylius@gmail.com>
2023-09-04 11:47:34 -0500

    1.1.2

--- src/main/java/lifesteal/lifestealcore/Commands/Withdraw.java
+++ src/main/java/lifesteal/lifestealcore/Commands/Withdraw.java
@@ -1,4 +1,13 @@
 package lifesteal.lifestealcore.Commands;
 
-public class Withdraw {
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Withdraw implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String [] args){
+        return true;
+    }
 }
--- src/main/java/lifesteal/lifestealcore/LifeStealCore.java
+++ src/main/java/lifesteal/lifestealcore/LifeStealCore.java
@@ -1,25 +1,38 @@
 package lifesteal.lifestealcore;
 
+import lifesteal.lifestealcore.Commands.Withdraw;
 import lifesteal.lifestealcore.itemmanagers.ItemManagerBeaconOfLife;
 import lifesteal.lifestealcore.itemmanagers.ItemManagerHeart;
 import lifesteal.lifestealcore.itemmanagers.ItemManagerHeartFrag;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import static org.bukkit.Bukkit.getPluginCommand;
+
 public final class LifeStealCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info("Enabling LifeSteal");
+        Bukkit.getLogger().info("§6Enabling LifeSteal");
         new PlayerDeath(this);
         new EquipHeart(this);
         ItemManagerHeartFrag.init();
         ItemManagerHeart.init();
         ItemManagerBeaconOfLife.init();
+        Bukkit.getPluginCommand("withdraw").setExecutor(new Withdraw());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info(" ");
+        Bukkit.getLogger().info("§6 Disabling LifeSteal");
     }
 }
